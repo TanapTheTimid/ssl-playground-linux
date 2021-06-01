c9dd8e3707d3d0da9188bf725da57a967538e835
TanapTheTimid <tanapoom.se@gmail.com>
2021-06-01 23:01:08 +0900

    you can now use normal youtube url

--- groovyshit/py_scripts/get_url_from_video_id.py
+++ groovyshit/py_scripts/get_url_from_video_id.py
@@ -1,8 +1,6 @@
 import sys
 import pafy
 
-YOUTUBE_VIDEO_URL_PREFIX = "https://www.youtube.com/watch?v="
-
-vid = pafy.new(YOUTUBE_VIDEO_URL_PREFIX + sys.argv[1])
+vid = pafy.new(sys.argv[1])
 print(vid.getbestaudio(preftype="m4a").url, flush=True)
 print(vid.title, flush=True)
